16e9887ac9ac90e278dd085f9095d447370376ab
Pawel Kuniec <pawllo1992@vp.pl>
2022-06-10 21:32:49 +0200

    webside-extension

--- Src/js/Historia.js
+++ Src/js/Historia.js
@@ -1,15 +1,14 @@
-alert('Szczęść Boże!')
 const masonry = new Macy({
     container: '.gallery',
     mobilefirst: true,
-    columns: 3,
+    rows: 3,
     breakAT: {
         400: 2,
         700: 3,
         1100: 4,
     },
     margin: {
-        x: 10,
+        x: 20,
         y: 40,
     }
 })
