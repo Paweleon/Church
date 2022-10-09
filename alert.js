b1d3ef20a38d75dc2f3b515c43eddf26a6de9394
Pawel Kuniec <pawllo1992@vp.pl>
2022-10-09 13:24:28 +0200

    alert

--- alert.js
+++ alert.js
@@ -1,9 +1,5 @@
 alert("Szczęść Boże!")
 
-if (confirm("Czy wyrzekasz się szatana?")) {
-    alert("Wchodź śmiało");
-    } else {
-    alert("Będziesz się smażył w piekle");
-  } 
+
 
   ellipse(203, 192, 300, 350);
